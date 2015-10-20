478b6f8911e37b7ffbb533ff1c867d487a3dcdf6
Felix Geller <fgeller@gmail.com>
2015-10-21 10:40:34 +1300

    Pulls future creation out of comprehension.

--- Main.scala
+++ Main.scala
@@ -5,20 +5,22 @@ object Main extends App {
   def log(msg: String) = println(s"${Thread.currentThread}: $msg")
 
   def readFile(fn: String): Future[Map[String, Int]] =
-    Future{
+    Future {
       log("reading file")
       Map("hans" -> 21, "peter" -> 42)
     }
   def updateCounts(mp: Map[String, Int]): Future[Map[String, Int]] =
-    Future{
+    Future {
       log("updating counts")
       mp.map({ case (n, c) => n -> (c + Random.nextInt) }).toMap
     }
 
+  val fc: Future[Map[String, Int]] = readFile("stats.txt")
+
   log("about to start reading")
   val f: Future[Map[String, Int]] =
     for {
-      oc <- readFile("stats.txt")
+      oc <- fc
       uc <- updateCounts(oc)
     } yield uc
 
