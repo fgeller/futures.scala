e416c4840897ebd1e32a9155f73aebc8694019b3
Felix Geller <fgeller@gmail.com>
2015-10-21 10:20:46 +1300

    Adds logging.

--- Main.scala
+++ Main.scala
@@ -1,11 +1,10 @@
 object Main extends App {
+  def log(msg: String) = println(s"${Thread.currentThread}: $msg")
   class Future[T](value: T)
   object Future {
-    def apply[T](v: T): Future[T] = {
-      new Future(v)
-    }
+    def apply[T](v: T): Future[T] = { new Future(v) }
   }
-  println("hello, world.")
-  val of: Future[Unit] = Future.apply(println("blubb"))
-  println("good bye.")
+  log("hello, world.")
+  val of: Future[Unit] = Future.apply(log("blubb"))
+  log("good bye.")
 }
