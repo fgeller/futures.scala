c22db4008e8f4b7078ea28ddd8eba6724c80f171
Felix Geller <fgeller@gmail.com>
2015-10-21 10:43:34 +1300

    Introduces Future.traverse

--- Main.scala
+++ Main.scala
@@ -6,14 +6,15 @@ object Main extends App {
   val data: Seq[Int] = Seq.fill(10)(Random.nextInt)
   val partitions: Seq[Seq[Int]]  = data.grouped(2).toSeq
   def work(is: Seq[Int]): Future[Seq[Int]] =
-    Future(is.map(_ / (Random.nextInt % 2)))
+    Future(is.map(_ / (Random.nextInt % 3)))
   val positives: Seq[Future[Seq[Int]]] = partitions.map(work)
-  val combined: Future[Seq[Seq[Int]]] = Future.sequence(positives)
-  val sum: Future[Long] =
-    combined.map(iss => iss.flatten.foldLeft(0L){ (acc, i) => acc + i })
+  val combined: Future[Seq[Try[Seq[Int]]]] =
+    Future.traverse(positives){ is: Future[Seq[Int]] =>
+      is.map(Success(_)).recover({ case th => Failure(th) })
+    }
 
-  sum andThen {
-    case Success(ps) => println(s"Success! $ps")
+  combined andThen {
+    case Success(ps) => println(s"Success! ${ps.toList}")
     case Failure(th) => println(s"Failure! $th")
   }
   Thread.sleep(10)
