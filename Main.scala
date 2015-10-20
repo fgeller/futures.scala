602218999b3e7071fd39811f1ad04a72fd70d931
Felix Geller <fgeller@gmail.com>
2015-10-21 10:21:55 +1300

    Starts another thread.

--- Main.scala
+++ Main.scala
@@ -1,8 +1,15 @@
 object Main extends App {
   def log(msg: String) = println(s"${Thread.currentThread}: $msg")
-  class Future[T](value: T)
+  class Future[T]() { var value: T = _ }
   object Future {
-    def apply[T](v: T): Future[T] = { new Future(v) }
+    def apply[T](v: T): Future[T] = {
+      val result = new Future[T]()
+      val thread = new Thread() {
+        override def run(): Unit = { result.value = v }
+      }
+      thread.start()
+      result
+    }
   }
   log("hello, world.")
   val of: Future[Unit] = Future.apply(log("blubb"))
